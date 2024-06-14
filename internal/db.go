434279535a604e4bf6eba735a03b8ea4ae4a54a9
MateoCaicedoW <mcaicedo@wawand.co>
2024-06-14 08:22:59 -0500

    chore: Update database package to use libsql-client-go for database connection

--- internal/db.go
+++ internal/db.go
@@ -6,17 +6,7 @@ import (
 	"os"
 
 	"github.com/tursodatabase/libsql-client-go/libsql"
-	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
-
-// func DatabaseConnection() (*sql.DB, error) {
-// 	db, err := sql.Open("libsql", os.Getenv("DATABASE_URL"))
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	return db, nil
-// }
 
 func DatabaseConnection() (*sql.DB, error) {
 	connector, err := libsql.NewConnector(os.Getenv("TURSO_DATABASE_URL"), libsql.WithAuthToken(os.Getenv("TURSO_AUTH_TOKEN")))
